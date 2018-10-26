95c73eb57a37c6457ec839c9376969e8ac56c062
glszKK <43538743+glszKK@users.noreply.github.com>
2018-10-26 16:59:29 +0200

    Update drill4.cpp

--- drill4.cpp
+++ drill4.cpp
@@ -2,23 +2,19 @@
 
 int main(){
 
-	// 1. drill
 	cout<<"Enter the name of the person you want to write to:\n";
 	String first_name;
 	cin >> first_name;
 	cout<<"Dear "<< first_name <<",\n";
 
-	//2. drill kinda
 	cout<<"How are you? I am fine. I miss you. \n";
 
 
-	//3. drill
 	String friend_name;
 	cout<<"Tell me someone from our friend group:\n";
 	cin>> friend_name;
 	cout<<"Have you seen "<<friend_name<<" lately?\n";
 
-	//4. drill
 	char friend_sex;
 	cout<<"Could you tell me "<<friend_name<<"'s sex? If male write \"m\", if female write \"f\" : \n";
 	cin>>friend_sex;
@@ -31,7 +27,6 @@ int main(){
 		cout<<"If you see "<<friend_name<<" please ask him to call me!\n";
 	}
 
-	//5. drill
 	cout<<"How old are you ?\n";
 	int age;
 	cin>>age;
@@ -45,7 +40,6 @@ int main(){
 		cout<<"I hear you just had a birthday and you are "<<age<<" years old!\n";
 	}
 
-	// kiegeszites
 
 	if(age < 12 )
 	{
